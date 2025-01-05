197f3e9c23a7b2af2e71117dc3bc6008a9882058
James Hodgkinson <james@terminaloutcomes.com>
2025-01-06 09:55:59 +1000

    fix(config/cli): cleanup of config/cli things

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,20 @@
 //! CLI Options
 
+use std::net::IpAddr;
 use std::path::PathBuf;
 
 use clap::Parser;
 
-#[derive(Parser, Debug, Default)]
+static DEFAULT_BIND_ADDRESS: &str = "::1";
+static DEFAULT_CONFIG_PATH: &str = "filekid.json";
+
+#[derive(Parser, Debug)]
 pub struct CliOpts {
-    #[clap(short, long, env = "FILEKID_CONFIG")]
-    pub config: Option<PathBuf>,
+    #[clap(short, long, env = "FILEKID_CONFIG", default_value = DEFAULT_CONFIG_PATH)]
+    pub config: PathBuf,
+
+    #[clap(long, env = "FILEKID_BIND_ADDRESS", default_value = DEFAULT_BIND_ADDRESS)]
+    pub bind_address: IpAddr,
 
     #[clap(short, long, env = "FILEKID_DEBUG")]
     pub debug: bool,
@@ -24,7 +31,25 @@ impl CliOpts {
     #[cfg(test)]
     pub fn test_default() -> Self {
         let mut opts = Self::default();
-        opts.config = Some(PathBuf::from("files/example-config.json"));
+        opts.config = PathBuf::from("files/example-config.json");
         opts
     }
 }
+
+impl Default for CliOpts {
+    fn default() -> Self {
+        #[allow(clippy::expect_used)]
+        Self {
+            config: PathBuf::from(DEFAULT_CONFIG_PATH),
+            debug: false,
+            #[cfg(any(debug_assertions, test))]
+            oauth2_disable: false,
+            db_debug: false,
+            bind_address: DEFAULT_BIND_ADDRESS
+                .parse()
+                .expect("Failed to parse ::1 as an IP address!"),
+        }
+    }
+}
+
+impl CliOpts {}
